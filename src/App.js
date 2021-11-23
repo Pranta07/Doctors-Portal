5f9d0aa696dbd244fc59c7f646cfe775a78d8860
Pranta07 <palpranta@gmail.com>
2021-11-24 02:06:00 +0600

    route setup done and navbar added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Dashboard from "./Pages/Dashboard/Dashboard/Dashboard";
+import Appointments from "./Pages/Appointments/Appointments";
+import MyAppointments from "./Pages/Dashboard/MyAppointments/MyAppointments";
+import Pay from "./Pages/Dashboard/Pay/Pay";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="home" element={<Home />} />
+                    <Route path="appointments" element={<Appointments />} />
+                    <Route path="dashboard" element={<Dashboard />}>
+                        <Route
+                            path="myAppointments"
+                            element={<MyAppointments />}
+                        />
+                        <Route path="pay" element={<Pay />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Pages/Appointments/Appointments.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navigation from "../../Shared/Navigation/Navigation";
+
+const Appointments = () => {
+    return (
+        <div>
+            <Navigation />
+            <h1>This is Appointments Page</h1>
+        </div>
+    );
+};
+
+export default Appointments;
