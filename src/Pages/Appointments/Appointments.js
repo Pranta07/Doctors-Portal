89c4afbb421314672527856a58d2f5ddf6a2ba96
Pranta07 <palpranta@gmail.com>
2021-11-24 15:58:41 +0600

    doctor-appointments section done

--- src/Pages/Appointments/Appointments.js
+++ src/Pages/Appointments/Appointments.js
@@ -1,8 +1,39 @@
+import { CalendarToday } from "@mui/icons-material";
+import { Typography } from "@mui/material";
+import { Paper } from "@mui/material";
+import { Button } from "@mui/material";
 import { Container, Divider, Grid } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
 import Calendar from "../../Shared/Calendar/Calendar";
 import Navigation from "../../Shared/Navigation/Navigation";
+
+const doctors = [
+    {
+        name: "Matthew Reyes",
+        field: "Obstetrics & Gynaecology",
+        desc: "University of California San Francisco Parnassus Campus",
+        available: ["Sat", "Mon", "Wed"],
+    },
+    {
+        name: "Rebecca Gilbert",
+        field: "Medicine Specialist",
+        desc: "University of San Francisco Parnassus Campus",
+        available: ["Sun", "Tue", "Thu"],
+    },
+    {
+        name: "Bobby Stanley",
+        field: "Dermatologist",
+        desc: "University of California San Francisco Parnassus Campus",
+        available: ["Sat", "Mon", "Wed"],
+    },
+    {
+        name: "Kathryn Cooper",
+        field: "Cardio Specialist",
+        desc: "University of California San Francisco Parnassus Campus",
+        available: ["Sun", "Tue", "Thu"],
+    },
+];
 
 const Appointments = () => {
     const [date, setDate] = React.useState(new Date());
@@ -10,6 +41,7 @@ const Appointments = () => {
     return (
         <div>
             <Navigation />
+
             <h1>Select Date to Book Appointments</h1>
             <Divider />
             <Container sx={{ py: 3 }}>
@@ -32,7 +64,72 @@ const Appointments = () => {
                     </Grid>
                 </Grid>
             </Container>
-            <h1>Available Appointments on {date.toDateString()}</h1>
+
+            <h1>Available Doctors on {date.toDateString()}</h1>
+            <Divider />
+            <Container sx={{ py: 3 }}>
+                <Grid
+                    container
+                    spacing={{ xs: 2, md: 3 }}
+                    columns={{ xs: 4, sm: 8, md: 12 }}
+                >
+                    {doctors.map((doctor) => (
+                        <Grid item xs={4} sm={4} md={4}>
+                            <Paper sx={{ p: 3 }}>
+                                <Typography variant="h5">
+                                    <Box
+                                        sx={{
+                                            fontWeight: "bold",
+                                            fontFamily: "Monospace",
+                                            m: 1,
+                                        }}
+                                    >
+                                        {doctor.name}
+                                    </Box>
+                                </Typography>
+                                <Typography>
+                                    <Box
+                                        sx={{
+                                            fontFamily: "Monospace",
+                                            fontWeight: "bold",
+                                            m: 1,
+                                        }}
+                                    >
+                                        {doctor.field}
+                                    </Box>
+                                </Typography>
+                                <Typography>
+                                    <Box
+                                        sx={{
+                                            fontFamily: "Monospace",
+                                            fontWeight: "light",
+                                            m: 1,
+                                        }}
+                                    >
+                                        {doctor.desc}
+                                    </Box>
+                                </Typography>
+                                <CalendarToday></CalendarToday>
+                                <Typography>
+                                    <Box
+                                        sx={{
+                                            fontFamily: "Monospace",
+                                            fontWeight: "bold",
+                                        }}
+                                    >
+                                        {doctor.available.map((day) => (
+                                            <span>{day} </span>
+                                        ))}
+                                    </Box>
+                                </Typography>
+                                <Button variant="contained" sx={{ my: 1 }}>
+                                    Book Appointment
+                                </Button>
+                            </Paper>
+                        </Grid>
+                    ))}
+                </Grid>
+            </Container>
         </div>
     );
 };
